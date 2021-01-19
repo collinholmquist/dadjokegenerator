02c1cf5ec6a639c56b084e61d139176b73891764
Collin Holmquist <collinholmquist@gmail.com>
2021-01-18 22:01:41 -0600

    add addition search option

--- index.js
+++ index.js
@@ -30,7 +30,20 @@ const getJokes = async () => {
     }
 }
 
-//READ
+const searchJokes = async (phrase) => {
+
+    try{
+        return await axios.get('https://icanhazdadjoke.com/search?term=' + phrase, {
+            headers: {
+                'Accept': 'application/json'
+
+            }})
+    } catch(error) {
+        console.error(error)
+    }   
+}
+
+//READ - render all Jokes from db
 app.get('/', (req, res) => {
 
     Joke.find({}, (err, jokes) => {
@@ -39,21 +52,14 @@ app.get('/', (req, res) => {
 
 })
 
-//CREATE
+//CREATE - create
 app.post('/', async (req, res) => {
 
-    //if req.body.content is empty, create a new joke by randomly grabbing one from the API. 
+    //console.log(req.body.content, 'from /')
+
     try{
-    
         const oneJoke = new Joke({})
-
-        if(typeof req.body.content !== 'undefined') {
-                oneJoke.content =  req.body.content
-        } else{
-            const randomJoke = await getJokes();
-            oneJoke.content = randomJoke.data.joke
-        }
-
+        oneJoke.content = req.body.content
         await oneJoke.save()
         res.redirect('/')
     } catch (err) {
@@ -61,9 +67,36 @@ app.post('/', async (req, res) => {
     }
 })
 
+app.post('/search', async(req, res) =>{
+
+    //console.log(req.body.content,' from /search')
+    //console.log(typeof req.body.content)
+    try{
+        const oneJoke = new Joke({})
+        
+        if(req.body.content === '') {
+                //console.log('we are getting a random joke')
+                const randomJoke = await getJokes();
+                oneJoke.content = randomJoke.data.joke
+        } else {
+                const searchedJoke = await searchJokes(req.body.content)
+                //console.log(searchedJoke.data)
+                oneJoke.content = searchedJoke.data.results[Math.floor(Math.random() * searchedJoke.data.results.length)].joke
+        
+        }
+        await oneJoke.save()
+        res.redirect('/')
+
+    } catch(err) {
+        res.redirect('/')
+    }
+        
+})
+
 //UPDATE 
 
-app.route("/edit/:id").get((req,res) => {
+app.route("/edit/:id")
+    .get((req,res) => {
 
         const id = req.params.id;
 
@@ -83,7 +116,9 @@ app.route("/edit/:id").get((req,res) => {
 
 //DELETE
 
-app.route("/remove/:id").get((req, res) => {
+app.route("/remove/:id")
+    
+.get((req, res) => {
     const id = req.params.id;
 
     Joke.findByIdAndRemove(id, err => {
